2a30b20de26b7569a5a8fc6f0e732505289788de
p0m1k <71141166+Zmrzlinka-coder@users.noreply.github.com>
2023-12-18 10:54:28 +0100

    Update linear_combination.py

--- src/math/linear_combination.py
+++ src/math/linear_combination.py
@@ -1,21 +1,31 @@
 from sympy.abc import *
 from sympy import Matrix, Symbol,shape,linsolve
 
-def linear_combination():
+class LinearCombination:
+    def __init__(self,vectors,equal_vector):
+        self.vectors=vectors
+        self.equal_vector=equal_vector
+    def calculate(self):
+        self.vectors=Matrix(self.vectors)
+        self.M = Matrix(self.equal_vector)
 
-    vectors=Matrix([[1, -1, 3],[1, 0, 1],[0, 1, 1]])
-    len_vectors=shape(vectors)[0]
-    k=[]
-    for i in range(0,len_vectors):
-        k.append(Symbol(f'k{i}'))
+        self.len_vectors=shape(self.vectors)[0]
 
-    M = Matrix([[3, -2, 4]])
-    multiplied_vectors=[list(vectors.row(i) *k[i]) for i in range(0,len_vectors)]
-    vectors2=Matrix(multiplied_vectors)
-    matrix=Matrix([list(vectors2.col(i)) for i in range(0,len_vectors)])
-    roots=linsolve([matrix, M])
-    print(roots)
+        self.k=[Symbol(f'k{i+1}') for i in range(0,self.len_vectors)]
 
-linear_combination()
+        self.multiplied_vectors=[list(self.vectors.row(i) *self.k[i]) for i in range(0,self.len_vectors)]
+        print(f'{self.equal_vector}={self.multiplied_vectors}')
+        
+        self.vectors2=Matrix(self.multiplied_vectors)
+        self.matrix=Matrix([list(self.vectors2.col(i)) for i in range(0,self.len_vectors)])
+        print(self.matrix)
+        self.roots=linsolve([self.matrix, self.M])
+        print(self.roots)
+
+if __name__=='__main__':
+    linear_combination=LinearCombination([[1, -2, 1],[-1, 1, 0],[1, 4, 2]],[[1, 3, 3]]).calculate()
+    linear_combination=LinearCombination([[1, -1, 3],[1, 0, 1],[0, 1, 1]],[[3, -2, 4]]).calculate()
+    linear_combination=LinearCombination([[1, 1, 3],[-1, 0, -2],[2, 2, 7]],[[1, 1, 2]]).calculate()
+    linear_combination=LinearCombination([[1, -2, 1],[-1, 1, 0],[1, 4, 2]],[[1, 3, 3]]).calculate()
 
 

30b547f6b052de084c40e161a7c8433a63a082d1
p0m1k <71141166+Zmrzlinka-coder@users.noreply.github.com>
2023-12-20 13:51:30 +0100

    Вывод ботом решений заданий
    Теперь выводит решение локальных экстрем и двойного интеграла

--- src/handlers/subject_task.py
+++ src/handlers/subject_task.py
@@ -4,7 +4,8 @@ from aiogram.types import Message, ReplyKeyboardRemove,callback_query
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from vsb_math.extrems import Extrems
-from strings.ru import solved_extrem
+from vsb_math.double_integral import DoubleIntegral
+from strings.ru import *
  
 from keyboards.for_subject import get_subject_kb
 from keyboards.for_task import get_task_kb
@@ -12,7 +13,8 @@ from keyboards.for_task import get_task_kb
 class SolveTask(StatesGroup):
     choosing_subject = State()
     choosing_task = State()
-    input_task=State()
+    input_extrem=State()
+    input_double_integral=State()
 
 router = Router()
 
@@ -44,13 +46,13 @@ async def incorrect_subject(message: Message):
 
 @router.callback_query(SolveTask.choosing_task,F.data=='double_integral')
 async def double_integral_input(callback:callback_query,state:FSMContext):
-    await callback.message.answer('Введи функцию и интервалы:')
-    await state.set_state(SolveTask.input_task)
+    await callback.message.answer('Введи функцию и интервалы(разделенные пробелом):')
+    await state.set_state(SolveTask.input_double_integral)
 
 @router.callback_query(SolveTask.choosing_task,F.data=='extrem')
 async def extrem_input(callback:callback_query,state:FSMContext):
     await callback.message.answer('Введи функцию:')
-    await state.set_state(SolveTask.input_task)
+    await state.set_state(SolveTask.input_extrem)
 
 @router.message(SolveTask.choosing_task)
 async def incorrect_task(message: Message):
@@ -59,7 +61,7 @@ async def incorrect_task(message: Message):
         reply_markup=get_task_kb()
     )
 
-@router.message(SolveTask.input_task)
+@router.message(SolveTask.input_extrem)
 async def extrem_output(message:Message,state:FSMContext):
     await message.answer_sticker(r'CAACAgIAAxkBAAJpOWWBnQpNP4BacSk_fc3Z2d5Xev1KAALUFAAC8i2ZSFHAjhpo6zLnMwQ')
     extrem=Extrems(message.text)
@@ -73,7 +75,42 @@ async def extrem_output(message:Message,state:FSMContext):
         derivate_y_y=extrem.deff_y_y,
         matrixes=extrem.matrixes
         ))
+    await validate_extrem(message,extrem)
+    
     await state.clear()
 
+@router.message(SolveTask.input_double_integral)
+async def extrem_output(message:Message,state:FSMContext):
+    await message.answer_sticker(r'CAACAgIAAxkBAAJpOWWBnQpNP4BacSk_fc3Z2d5Xev1KAALUFAAC8i2ZSFHAjhpo6zLnMwQ')
+    args=validate_double_integral(message.text)
+    double_integral=DoubleIntegral(args[0],args[1],args[2])
+    double_integral.calculate()
+    await message.answer(solved_double_integral.format(
+        integrate_y=double_integral.integrate_y,
+        upper_sub_y=double_integral.upper_sub_y,
+        lower_sub_y=double_integral.lower_sub_y,
+        difference_y=double_integral.subtract_y,
+        integrate_x=double_integral.integrate_x,
+        upper_sub_x=double_integral.upper_sub_x,
+        lower_sub_x=double_integral.lower_sub_x,
+        difference_x=double_integral.subtract_x
+    ))
+    await state.clear()
+
+def validate_double_integral(str):
+    return str.split()
+
+async def validate_extrem(message:Message,extrem):
+    index = 0
+    for matrix in extrem.matrixes:
+        delta2 = matrix[0] * matrix[3] - matrix[1] * matrix[2]
+        delta1 = matrix[0]
+        if delta2 > 0:
+            await message.answer(local_min.format(min=extrem.roots[index])) if delta1>0 else await message.anwser(local_max.format(max=extrem.roots[index]))
+        elif delta2 < 0:
+            await message.answer(no_extrem.format(no_extr=extrem.roots[index]))
+        else:
+            await message.answer(no_solution)
+        index += 1  
     
 
